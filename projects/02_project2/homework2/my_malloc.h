ef1bb319e2b02142ae12b4a8f8ac47a3e1d9dbff
jiyuan.lyu@duke.edu <jl1230@duke.edu>
2024-01-30 15:46:59 -0500

    bf malloc for the hw2

---
+++ projects/02_project2/homework2/my_malloc.h
@@ -0,0 +1,28 @@
+#ifndef __MY_MALLOC_H__
+#define __MY_MALLOC_H__
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct Node {
+    // this size include the metadata size (the node size)
+    size_t sum_size;
+    struct Node *prev;
+    struct Node *next;
+} Node;
+void printLL(Node *head);
+
+void split(Node *curr, size_t size);
+void merge(Node *curr);
+
+// Best Fit malloc/free
+void * bf_malloc(size_t size);
+void bf_free(void *ptr);
+
+// check function will return NULL if there is no fit
+// it will return a pointer to the fitted Node if there is a fit
+Node *checkBF(size_t size);
+
+#endif
